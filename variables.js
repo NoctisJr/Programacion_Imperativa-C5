f8a8c6fbbbe4a9ed9ef1d5420951556f70a3b74e
NoctisJr <juniorelilopez@gmail.com>
2023-08-01 21:56:56 -0500

    agrego actividad variables

---
+++ variables.js
@@ -0,0 +1,55 @@
+// Variables -> let y const 
+
+let edad = 25;
+const apellido = "Perez";
+
+console.log(edad, apellido);
+
+// Tipos de datos -> primitivos 
+
+// Sting -> cadena de texto 
+let nombre ="pepe";
+
+// Numbre -> numeros 
+let numeroDeLaSuerte = 22;
+
+// BOOLEAN -> true - false 
+let isLogged = true;
+
+// Undefine -> no definido
+let nombreDeUsuario = "";
+
+// Null
+let datos = null;
+
+// NaN -> not a number
+let X = NaN;
+
+// Operadores Matematicos
+
+let n1 = 2;
+let n2 = 3;
+
+console.log(n1+n2);
+console.log(n1-n2);
+console.log(n1*n2);
+console.log(n1/n2);
+//--Modulo 
+console.log(n1%n2);
+//concatenar 
+let saludo = "hola como estas"
+console.log(saludo + " Muy bien");
+//concatenar template literals -> backsticks 
+let saludoBien = `hola ${nombre} como estas?`
+
+// Operadores COMPARACION
+
+console.log(n1==n2); // comparacion simple
+console.log(n1===n2); // comparacion estricta
+console.log(n1!=n2);
+console.log(n1>n2);
+console.log(n1<n2);
+console.log(n1>=n2);
+console.log(n1<=n2);
+
+
