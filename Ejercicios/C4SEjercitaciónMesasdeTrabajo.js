f65c3e7f6e721d760b86670e08a9ad9f4d78b16d
NoctisJr <juniorelilopez@gmail.com>
2023-08-19 11:09:26 -0500

    Mesa de trabajo

---
+++ Ejercicios/C4SEjercitaciónMesasdeTrabajo.js
@@ -0,0 +1,27 @@
+// ****** Un dia de clases en DH ******
+// 1) Validar que la carga de bateria de mi laptop este en buenos niveles
+let bateriaLaptop = 90;
+// 2) Coordino una hora antes de clases para validar contenido de PG
+let horaAsincronica = 20;
+// 3) Validar horario de clases (hora EC/CO)
+let horaInicioClases = 21;
+// 4) Validar la materia a la que debemos ingresar
+let ProgramacionImperativa = true;
+let BasesDeDatos = false;
+// 5) Horario en el que termina las clases con el tutor y comienzan las mesas de trabajo (hora EC/CO)
+let horaFinClases = 22;
+let actividadMesaDeTrabajo = "";
+// 6) Si la actividad es terminada, se entrega
+actividadDeClases = true;
+
+
+// Desafío extra:
+let cajonPantalones = "remeras";
+let cajonRemeras = "";
+let cajon = `Tengo mis ${cajonPantalones} en el cajon de Pantalones`;
+console.log(cajon);
+
+cajonPantalones = "";
+cajonRemeras = "remeras";
+cajon = `Ahora coloque mis ${cajonRemeras} en el cajon correspondiente`;
+console.log(cajon);
